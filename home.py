2db2aac5aa9137093253b17e5ef0e2511072c7a1
laksath <166405335+laksath-affine@users.noreply.github.com>
2025-01-19 20:26:13 +0530

    rename

--- home.py
+++ home.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 
 def home_page():
-    st.title("Welcome to Open Wardrobe!")
+    st.title("Welcome to Outfit Wizard!")
     st.write("""
-        Open Wardrobe is your personal digital wardrobe that helps you 
+        Outfit Wizard is your personal digital wardrobe that helps you 
         manage your fashion items with ease. You can upload images of your 
         clothing items, add descriptions, and organize them into different wardrobes.
     """)
